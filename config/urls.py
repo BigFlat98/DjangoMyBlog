4cd0c16622a9d30122b36beaf990e5ac1962d810
BigFlat98 <hdj9836@naver.com>
2024-11-05 11:49:23 +0900

    middle commit

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import HomeView
 from blog import urls as blog_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',HomeView.as_view(),name='home'), #views에서 확인 가능. 클래스 뷰를 사용해서 내장 함수를 사용했음.
     path('blog/',include(blog_urls)),
 ]
+
+#post 모델에서 이미지를 넣을 때 이미지가 저장되는 위치 설정.
+#config -> settings.py 에서 media경로 수정하고 url패턴 변경
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
